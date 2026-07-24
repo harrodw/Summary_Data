c96d139f95bbb0d98ddef4eb5447a64ea702ce75
Will Harrod <wdharrod@ncsu.edu>
2026-07-24 13:35:08 -0400

    Added bird ARU code

--- Scripts/Calculating_Effort.R
+++ Scripts/Calculating_Effort.R
@@ -17,8 +17,8 @@ library(tidyverse)
 library(fs)
 
 # Directories
-bird_frog_dir <- "G:/Bird_Anuran_ARUs"
-bat_dir <- "G:/Bat_ARUs"
+bird_frog_dir <- "D:/Bird_Anuran_ARUs"
+bat_dir <- "D:/Bat_ARUs"
 
 ################################################################################
 # 2: Bird and Anuran ARU Effort ################################################
@@ -26,26 +26,72 @@ bat_dir <- "G:/Bat_ARUs"
 
 # List the ARUS
 aru_ls <- dir_ls(bird_frog_dir)
+n_aru <- length(aru_ls)
 
 # View
 aru_ls
-length(aru_ls)
+n_aru
 
 # View info for the first aru
-single_aru_info <- aru_ls[1] |>
+aru_info <- aru_ls[4] |>
   dir_info() |>
   mutate(file = basename(path)) |>
-  mutate(file = str_remove_all(file, "SMM2-\\d{2}_")) |>
-  mutate(file = str_remove_all(file, "\\.wav")) |>
-  mutate(date = str_extract(file, "\\d{8}_"),
-         time = str_extract(file, "_\\d{6}")) |>
-  mutate(date = str_remove_all(date, "_"),
-         time = str_remove_all(time, "_")) |>
-  # mutate(date = ymd(date),
-  #        time = hms(time)) |>
-  select(file, date, time, size)
+  mutate(file.name = str_remove_all(file, "SMM2-\\d{2}_")) |>
+  mutate(file.name = str_remove_all(file.name, "\\.wav")) |>
+  mutate(file.name = str_remove_all(file.name, "_")) |> 
+  mutate(date.time = parse_date_time(file.name, orders = "ymd%H%M%S")) |> 
+  select(date.time, size, file)
 
-glimpse(single_aru_info)
+# View
+glimpse(aru_info)
+head(aru_info)
+# what is the range of file sizes
+aru_info |> 
+  count(size) |> 
+  arrange(size) |> 
+  mutate(size = as.numeric(str_remove_all(as.character(size), "M")))
 
+# Large recordings are over 100 MB
+lrg_fil_sze <-  100
 
 # Blank data frame to store the number of morning recordings 
+rec_info <- tibble(
+)
+
+# Loop over the ARUs and extract the relevant information
+for(i in 1:n_aru){
+
+  # Define a single ARU
+  aru <- aru_ls[i]
+  plot <- basename(aru)
+  
+  # List the recordings made by that ARU by size and extract the important information
+  recs <- aru|>
+    dir_info() |>
+    mutate(file = basename(path),
+           Plot = plot) |>
+    mutate(file.name = str_remove_all(file, "SMM2-\\d{2}_")) |>
+    mutate(file.name = str_remove_all(file.name, "\\.wav")) |>
+    mutate(file.name = str_remove_all(file.name, "_")) |> 
+    mutate(date.time = parse_date_time(file.name, orders = "ymd%H%M%S")) |> 
+    mutate(size = as.numeric(str_remove_all(as.character(size), "M"))) |> 
+    mutate(Morning = case_when(
+      size > lrg_fil_sze ~ 1,
+      size <= lrg_fil_sze ~ 0
+    )) |> 
+    select(Plot, date.time, Morning, size, file)
+  
+  # Combine with the others
+  rec_info <- bind_rows(rec_info, recs)
+  
+  # Message
+  message("Finished extracting info for  ", plot, " Plot ", i, " out of ", n_aru)
+}
+
+# View
+glimpse(rec_info)
+
+# Where are the NA's?
+rec_info |> 
+  filter(if_any(everything(), is.na))
+
